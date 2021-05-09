5ee11936d64bee44bb9c1377f00594f30b99da73
Angelo Pimienta <33708611+numbers1234567@users.noreply.github.com>
2021-05-09 14:13:21 -0500

    Fixed the invisible text issue. Added generic board class generator

---
+++ MainGame/board_definitions.py
@@ -0,0 +1,45 @@
+from pyglet.gl import GL_TRIANGLES, GL_LINES
+from pyglet.text import Label as pyglLabel
+from display_genus_board import *
+
+"""
+Generate any arbitrary minesweeper board with genus
+"""
+def genus_generate_compatible_class(genus):
+    """Generate graphics class"""
+    class GenusGBoardGraphics(TileGraphicsContainer):
+        def __init__(self, tile, batch, text_batch):
+            # Get screen coordinates
+            self.tile_coords = [(coord[0]*self.SCALE+self.OFFSET[0], coord[1]*self.SCALE+self.OFFSET[1]) for coord in tile.coordinates]
+            # Store batches
+            self.batch = batch
+            self.text_batch = text_batch
+            # Skeleton
+            self.fill = batch.add(3, GL_TRIANGLES, None,
+                                ("v2f", tuple([*self.tile_coords[0],*self.tile_coords[1],*self.tile_coords[2]])),
+                                ("c3B", (0, 0, 255,0, 0, 255,0, 0, 255)))
+            self.borders = batch.add(6, GL_LINES, None, 
+                                    ("v2f", tuple([*self.tile_coords[0],*self.tile_coords[1],*self.tile_coords[1], *self.tile_coords[2], *self.tile_coords[2],*self.tile_coords[0]])),
+                                    ("c3B", (255,255,255)*6))
+
+            self.label = None
+            self.tile = tile
+            self.update_graphics()
+        
+        def update_graphics(self):
+            if self.tile.revealed:
+                self.fill.colors = [0, 0, 255,0, 0, 255,0, 0, 255]
+                if self.tile.get_num_adjacent_mines() > 0 and self.label==None: 
+                    centroidX = sum([coord[0] for coord in self.tile_coords])/3
+                    centroidY = sum([coord[1] for coord in self.tile_coords])/3
+                    self.label = pyglLabel(text=str(self.tile.get_num_adjacent_mines()), font_size=18, anchor_x="center", anchor_y="center",x=int(centroidX), y=int(centroidY), batch=self.text_batch)
+                    self.label.font_size=12
+                return
+            if self.tile.flagged: self.fill.colors = [0,0,0,0,0,0,0,0,0]
+            else: self.fill.colors = [150, 150, 150,150, 150, 150,150, 150, 150]
+    
+    class GenusGBoard(MSGenusGBoard):
+        def __init__(self, mine_locs):
+            super().__init__(mine_locs, genus)
+    
+    return (GenusGBoard, GenusGBoardGraphics)
--- MainGame/main.py
+++ MainGame/main.py
@@ -1,72 +1,42 @@
 import pyglet
 from msboard_genus_surfaces import *
 from display_genus_board import *
+from board_definitions import *
 from random import randint
 from math import cos
+import sys
 
 """
-BOARD DEFINITION
+Main game container
 """
-class Genus2Board(MSGenusGBoard):
-    def __init__(self, mine_locs):
-        super().__init__(mine_locs, 2)
-
-class Genus2BoardGraphics(TileGraphicsContainer):
-    OFFSET=(512,256)# Scale and then offset
-    SCALE=20
-    def __init__(self, tile, batch):
-        tile_coords = [(coord[0]*self.SCALE+self.OFFSET[0], coord[1]*self.SCALE+self.OFFSET[1]) for coord in tile.coordinates]
-        #print(tuple([*tile_coords[0],*tile_coords[1],*tile_coords[2]]))
-        self.fill = batch.add(3, pyglet.gl.GL_TRIANGLES, None,
-                              ("v2f", tuple([*tile_coords[0],*tile_coords[1],*tile_coords[2]])),
-                              ("c3B", (0, 0, 255,0, 0, 255,0, 0, 255)))
-        self.borders = batch.add(6, pyglet.gl.GL_LINES, None, 
-                                 ("v2f", tuple([*tile_coords[0],*tile_coords[1],*tile_coords[1], *tile_coords[2], *tile_coords[2],*tile_coords[0]])),
-                                 ("c3B", (255,255,255)*6))
-        centroidX = sum([coord[0] for coord in tile_coords])/3
-        centroidY = sum([coord[1] for coord in tile_coords])/3
-        self.label = pyglet.text.Label(text=" ", font_size=18, anchor_x="center", anchor_y="center",x=int(centroidX), y=int(centroidY), batch=batch)
-        self.label.font_size=12
-        #self.label = pyglet.text.Label(text=" ", font_name="Calibri", font_size=24, x=512, y=256, batch=batch)
-        self.tile = tile
-        self.update_graphics()
-    
-    def update_graphics(self):
-        if self.tile.revealed:
-            self.fill.colors = [0, 0, 255,0, 0, 255,0, 0, 255]
-            if self.tile.get_num_adjacent_mines != 0: self.label.text=str(self.tile.get_num_adjacent_mines())
-            return
-        if self.tile.flagged: self.fill.colors = [0,0,0,0,0,0,0,0,0]
-        else: self.fill.colors = [150, 150, 150,150, 150, 150,150, 150, 150]
-
-class GameContainer:
+class Game:
     CLICKREVEAL=1
     CLICKFLAG=2
     def __init__(self, mine_locs, board_type, graphics_container_type):
         self.display_batch = pyglet.graphics.Batch()
+        self.text_batch = pyglet.graphics.Batch()
         self.board = board_type(mine_locs)
-        self.graphics = [graphics_container_type(tile, self.display_batch) for tile in self.board.tiles]
+        self.graphics = [graphics_container_type(tile, self.display_batch, self.text_batch) for tile in self.board.tiles]
     
     def display(self):
         for tile_graphics in self.graphics:
             tile_graphics.update_graphics()
         self.display_batch.draw()
+        self.text_batch.draw()
 
     def on_click(self, x, y, type_):
         real_x=(x-self.graphics[0].OFFSET[0])/self.graphics[0].SCALE
         real_y=(y-self.graphics[0].OFFSET[1])/self.graphics[0].SCALE
-        if type_==GameContainer.CLICKREVEAL: return (self.board.on_click(real_x, real_y, self.board.CLICKREVEAL))
+        if type_==Game.CLICKREVEAL: return (self.board.on_click(real_x, real_y, self.board.CLICKREVEAL))
         return self.board.on_click(real_x, real_y, self.board.CLICKFLAG)
 
-class main(pyglet.window.Window):
+class Main(pyglet.window.Window):
     SCREENH=512
     SCREENW=1024
-    def __init__(self, board_type):
+    def __init__(self, board_type, board_graphics, mine_locs):
         super().__init__(self.SCREENW, self.SCREENH)
         pyglet.gl.glClearColor(0,0,0,1)
-        mine_locs = [randint(0, 4)==0 for i in range(800)]
-        for i in range(8):mine_locs[i] = False
-        self.game = GameContainer(mine_locs, Genus2Board, Genus2BoardGraphics)
+        self.game = Game(mine_locs, board_type, board_graphics)
 
     def on_draw(self):
         self.clear()
@@ -74,10 +44,15 @@ class main(pyglet.window.Window):
 
     def on_mouse_press(self, x, y, button, modifiers):
         if button == pyglet.window.mouse.LEFT:
-            self.game.on_click(x, y, GameContainer.CLICKREVEAL)
+            self.game.on_click(x, y, Game.CLICKREVEAL)
         if button == pyglet.window.mouse.RIGHT:
-            self.game.on_click(x, y, GameContainer.CLICKFLAG)
+            self.game.on_click(x, y, Game.CLICKFLAG)
 
 if __name__=="__main__":
-    window = main(Genus2Board)
+    board_type, board_graphics = genus_generate_compatible_class(int(sys.argv[1]))
+    board_graphics.SCALE=20
+    board_graphics.OFFSET = (512, 256)
+    mine_locs = [randint(0, 4)==0 for i in range(int(sys.argv[1])*4*25)]
+    for i in range(16):mine_locs[i] = False
+    window = Main(board_type, board_graphics, mine_locs)
     pyglet.app.run()
